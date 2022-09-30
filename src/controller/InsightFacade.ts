67fa8b077a0fbeeedf56f5cee10db6e2e421c8ce
Has Xi <aichex@students.cs.ubc.ca>
2022-09-29 17:50:00 -0700

    processCourses function added for addDataSet

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -7,6 +7,9 @@ import {
 	NotFoundError
 } from "./IInsightFacade";
 
+import jsZip from "jszip";
+import fs from "fs-extra";
+
 /**
  * This is the main programmatic entry point for the project.
  * Method documentation is in IInsightFacade
@@ -32,4 +35,41 @@ export default class InsightFacade implements IInsightFacade {
 	public listDatasets(): Promise<InsightDataset[]> {
 		return Promise.reject("Not implemented.");
 	}
+
+	private processCourses = async (zipFile: string): Promise<object[]> => {
+		const zip = await jsZip.loadAsync(zipFile, {base64: true});
+		let toBeProcessed: any[] = [];
+		let processed: object[] = [];
+		return new Promise((fullfill, reject) => {
+			for (let filePath in zip.files) {
+				console.log(filePath);
+				// if (filePath.match(`courses\/[^\.].*`)) {
+				let file = zip.file(filePath);
+				if (file) {
+					file.async("string").then((fileString) => {
+						toBeProcessed.push(fileString);
+					});
+				} else {
+					reject({InsightError});
+					 }
+				// }
+			}
+			for (let dataFile of toBeProcessed) {
+				// console.log(dataFile);
+				let jsonObject = JSON.parse(dataFile); // parse course if defined
+				// console.log(jsonObject);
+				processed.push(jsonObject);
+			}
+			fullfill(processed);
+		});
+	};
+
+	public processZip = async (zipFile: string, id: string): Promise<object> => {
+		const zip = await jsZip.loadAsync(zipFile, {base64: true});
+		console.log(zip);
+		console.log(id);
+		// await this.processCourses(zip);
+		return Promise.resolve({});
+	};
 }
+
