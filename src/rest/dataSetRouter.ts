a300d7e9bdfd24c54fda9d68c222df6f8bd0f83a
Has Xi <aichex@students.cs.ubc.ca>
2022-11-27 21:54:08 -0800

    adding routers

---
+++ src/rest/dataSetRouter.ts
@@ -0,0 +1,50 @@
+import InsightFacade from "../controller/InsightFacade";
+import {IInsightFacade, InsightDatasetKind} from "../controller/IInsightFacade";
+import {Request, Response} from "express";
+
+const facade: IInsightFacade = new InsightFacade();
+
+const addDatasetRouter = async (req: Request, res: Response) => {
+	let data = req.body.content;
+	let id = req.params.id;
+	let datasetType = req.params.kind;
+	let kind;
+	try {
+		if (datasetType === "rooms") {
+			kind = InsightDatasetKind.Rooms;
+		} else {
+			kind = InsightDatasetKind.Sections;
+		}
+		const result = await facade.addDataset(id, data, kind);
+		res.send(result);
+	} catch (e) {
+		res.status(500).json({
+			message: e
+		});
+	}
+};
+
+const removeDatasetRouter = async (req: Request, res: Response) => {
+	let id = req.params.id;
+	try {
+		const result = await facade.removeDataset(id);
+		res.send(result);
+	} catch (e) {
+		res.status(500).json({
+			message: e
+		});
+	}
+};
+
+const listDatasetRouter = async (res: Response) => {
+	try {
+		const response = await facade.listDatasets();
+		res.send(response);
+	} catch (e) {
+		res.status(500).json({
+			message: e
+		});
+	}
+};
+
+export {addDatasetRouter, removeDatasetRouter, listDatasetRouter};
