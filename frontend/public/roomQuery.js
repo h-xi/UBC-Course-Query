292581155677e7ec12a37001ea38399f34f54260
yitaaa <102426871+yitaaa@users.noreply.github.com>
2022-11-30 15:01:53 -0800

    add error alter forn front end room query

---
+++ frontend/public/roomQuery.js
@@ -0,0 +1,61 @@
+
+document.getElementById("searchRoomButton").addEventListener("click", handleSearchRoom);
+
+
+function handleSearchRoom() {
+	const classSize = parseFloat(document.getElementById("cSize").value);
+	console.log(classSize);
+	let roomQuery = createRoomQuery(classSize);
+	const httpRequest = new XMLHttpRequest();
+	httpRequest.responseType = "json"
+	httpRequest.onload = () => {
+		if (httpRequest.status === 400) {
+			// console.log(httpRequest.response);
+			alert(httpRequest.response.error);
+		} else {
+			// createTable(httpRequest.response.result)
+		}
+		// if (httpRequest.readyState === httpRequest.DONE) {
+		// 	if (httpRequest.status === 400) {
+		// 		// console.log(httpRequest.response);
+		// 		alert(httpRequest.response.error);
+		// 	} else {
+		// 		// createTable(httpRequest.response.result)
+		// 	}
+		// } else {
+		// 	// maybe do something
+		// }
+	}
+	httpRequest.open('POST', 'http://localhost:4321/query');
+	httpRequest.setRequestHeader('Content-Type', 'application/json');
+	httpRequest.send(JSON.stringify(roomQuery));
+}
+
+function createRoomQuery(classSize) {
+	let query = {
+		"WHERE": {
+			"AND": [
+				{
+					"IS": {
+						"rooms_furniture": "*Tables*"
+					}
+				},
+				{
+					"GT": {
+						"rooms_seats": classSize
+					}
+				}
+			]
+		},
+		"OPTIONS": {
+			"COLUMNS": [
+				"rooms_shortname",
+				"rooms_number",
+				"rooms_seats",
+				"rooms_href"
+			]
+		}
+	};
+	return query;
+}
+
